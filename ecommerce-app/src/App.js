67d3666f6065f8802de522fea4d4be29f7a9f244
Konstantin <kvckov@yahoo.com>
2021-10-20 13:55:23 +0200

    "Integrated Firebase into application,created sign in page with sign in component."

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import ShopPage from './pages/shopPage/ShopPage';
 import './App.css';
-import HomePage from './pages/HomePage/HomePage'
+import HomePage from './pages/HomePage/HomePage';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header-Nav/Header-comp';
+
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+       <Route path='/shop' component={ ShopPage } />
+      </Switch>
     </div>
   );
 }
--- ecommerce-app/src/components/menuItemComponent/menu-item.jsx
+++ ecommerce-app/src/components/menuItemComponent/menu-item.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import './menu-item.scss'
+import React from 'react';
+import { withRouter } from 'react-router';
+import './menu-item.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div  className={`${size} menu-item`}>
-        <div className='background-image'
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className='background-image'
         style={{
-            backgroundImage: `url(${imageUrl})`
+          backgroundImage: `url(${imageUrl})`
         }}
-        />
-    <div className="content">
-        <h1 className="title">{ title.toUpperCase() }</h1>
-        <span className="subtitle">Shop Now</span>
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-    </div>
-);
-
-export default MenuItem;
+  );
+  
+  export default withRouter(MenuItem);
